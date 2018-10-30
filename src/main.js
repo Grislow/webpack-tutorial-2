e69b1a3ea8d036206606ed2378a241695149fe6d
Grzegorz Maj <gmaj@inbrackets.pl>
2018-10-30 18:33:12 +0100

    Babel with preset env

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+require("babel-runtime/regenerator")
 require("./main.css");
 require("./index.html");
 
-var a = async () => {
+var a = async (args) => {
+    const { a, b } = args;
     await console.log('Hello World');
+    console.log('Then this');
 }
+
+a({a: 1, b: 2})
